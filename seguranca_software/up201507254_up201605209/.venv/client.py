9343a28432c0620438ae57f58d8de29adf0232e2
Pedro Antunes <50156060+antunesmpedro@users.noreply.github.com>
2022-11-09 13:35:49 +0000

    Delete seguranca_software directory

--- seguranca_software/up201507254_up201605209/.venv/client.py
+++
@@ -1,9 +0,0 @@
-import sys
-import json
-import requests
-
-conv = {'name': 'teste', 'password': 'Greeting'}
-s = json.dumps(conv) 
-print(s)
-res = requests.post("http://127.0.0.1:5000/user", json=s).json()
-print(res)
